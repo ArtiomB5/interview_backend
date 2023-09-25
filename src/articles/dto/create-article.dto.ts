969940d42aeaac1b3a879361dbfcbcecafb79a9a
ArtiomB5 <artiomb5@yande.by>
2023-09-26 00:22:01 +0600

    Input Validation & Transformation

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,18 +1,38 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty()
   question: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   answer: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   published: boolean;
 
+  @IsNotEmpty()
   @ApiProperty()
   author: string;
 }
